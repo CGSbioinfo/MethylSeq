f9ae18bf2b69a1d149943377fdbbcc2b557aef10
Maria <mjg225@zoidberg.hmg.path.cam.ac.uk>
2016-08-17 12:31:04 +0100

    scripts

--- bin/analysis_info.py
+++ bin/analysis_info.py
@@ -11,7 +11,7 @@ import logging
 from joblib import Parallel, delayed
 import multiprocessing
 import subprocess
-sys.path.insert(0,'.')
+#sys.path.insert(0,'/usr/local/bin/')
 import functions
 import argparse
 
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     """ This script creates a file which needs to be filled with information required for a methylSeq project.
     - It takes one argument, the 'outfile', which is the name of the output file. The default is 'analysis_info.txt'""" 
 
-    parser=argparse.ArgumentParser(prog='analysis_info.py')
+    parser=argparse.ArgumentParser(prog='analysis_info.py', description='Creates analysis_info.txt')
     parser.add_argument('-v','--version',action='version',version='%(prog)s-'+__version__)
     parser.add_argument('--outfile', help='Name of the output file. Default=analysis_info.txt', default='analysis_info.txt')
     args=parser.parse_args()
